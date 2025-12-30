a3f36a9449fcb6b396f43538234b9a86bb44b19b
Hikma1 <hikma0227@gmail.com>
2025-12-30 21:54:11 +0300

    bad

--- Searching/FirstBadVersion.py
+++ Searching/FirstBadVersion.py
@@ -2,8 +2,8 @@
 Docstring for Searching.FirstBadVersion
 Given n versions [1, 2, ..., n] and an API isBadVersion(version)
  which returns whether version is bad. You need to find the first bad version,
-  which causes all the following ones to be bad.
-
+  which causes all the following ones to be bad. You should minimize the number of calls to the API.
+  
 '''
 # The isBadVersion API is already defined for you.
 # @param version, an integer
