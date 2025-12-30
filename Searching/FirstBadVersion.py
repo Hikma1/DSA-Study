18f2d14530109664bf7a74a1cbf334328e72fa73
Hikma1 <hikma0227@gmail.com>
2025-12-30 21:51:49 +0300

    firstbadversion

--- Searching/FirstBadVersion.py
+++ Searching/FirstBadVersion.py
@@ -1,4 +1,27 @@
 '''
 Docstring for Searching.FirstBadVersion
+Given n versions [1, 2, ..., n] and an API isBadVersion(version) which returns whether version is bad.
 
 '''
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start = 1
+        while start<=n:
+            mid= (start+n)/2
+            if isBadVersion(mid):
+                n= mid-1
+            else:
+                start = mid +1
+        return start
+
+            
+        
