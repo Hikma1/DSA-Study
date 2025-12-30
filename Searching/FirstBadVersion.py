3b6d3931f043b880dcbec845fdcaf2003780c143
Hikma1 <hikma0227@gmail.com>
2025-12-30 22:13:02 +0300

    sorting

--- Searching/FirstBadVersion.py
+++ Searching/FirstBadVersion.py
@@ -5,6 +5,13 @@ Given n versions [1, 2, ..., n] and an API isBadVersion(version)
   which causes all the following ones to be bad. You should minimize the number of calls to the API.
   
 '''
+'''logic:
+We can use binary search to minimize the number of calls to the API.
+ We start with the entire range of versions from 1 to n. 
+ We calculate the mid-point of the current range and check if it is a bad version using the isBadVersion API.
+ If it is a bad version, we know that the first bad version must be at mid or before mid, so we adjust our search range to the left half by setting n to mid - 1.
+ If it is not a bad version, we know that the first bad version must be after mid, so we adjust our search range to the right half by setting start to mid + 1.
+ '''
 # The isBadVersion API is already defined for you.
 # @param version, an integer
 # @return a bool
