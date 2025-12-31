cf75301ec5f0c9da533f509c22a8d702c27a9469
Hikma1 <hikma0227@gmail.com>
2025-12-31 09:28:57 +0300

    dsa

---
+++ Sorting/SortByParity.py
@@ -0,0 +1,7 @@
+'''
+Docstring for Sorting.SortByParity
+'''
+'''logic for this problem:
+We can solve this problem by using a two-pointer approach. We will maintain two pointers, one starting from the beginning of the array and the other from the end of the array.
+ The left pointer will move rightwards until it finds an odd number, and the right pointer will move leftwards until it finds an even number.
+ When both pointers find their respective numbers, we will swap them. This process continues until the left'''
