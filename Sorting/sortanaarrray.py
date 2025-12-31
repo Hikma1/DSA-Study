4c702faefa11103f0bb5d4f8da311d7b04d2ff13
Hikma1 <hikma0227@gmail.com>
2025-12-31 09:36:28 +0300

    sorting

--- Sorting/sortanaarrray.py
+++ Sorting/sortanaarrray.py
@@ -9,3 +9,11 @@ logic for this problem:we can use the bubble sort algorithm to sort the array in
 We will iterate through the array multiple times, and in each iteration, we will compare adjacent elements
 and swap them if they are in the wrong order. This process will continue until the entire array is sorted.
 '''
+class Solution(object):
+    def sortArray(self, nums):
+       n= len(nums)
+       for i in range(n):
+        for j in range(0, n-i-1):
+            if nums[j]>nums[j+1]:
+                nums[j], nums[j+1]=nums[j+1],nums[j]
+       return nums
