1c3ee7ac1dc8d83d0a76f21054931b92ac1c6aec
Hikma1 <hikma0227@gmail.com>
2025-12-31 09:35:51 +0300

    sorting

--- Sorting/sortanaarrray.py
+++ Sorting/sortanaarrray.py
@@ -5,5 +5,7 @@ Given an array of integers nums, sort the array in ascending order and return it
 You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
 '''
 '''
-logic for this problem:
+logic for this problem:we can use the bubble sort algorithm to sort the array in ascending order.
+We will iterate through the array multiple times, and in each iteration, we will compare adjacent elements
+and swap them if they are in the wrong order. This process will continue until the entire array is sorted.
 '''
